3a01bc0dc3880c9983e5140c1aacd8548932c30b
Nikhil <nikhilcogni@gmail.com>
2024-02-04 11:11:06 +0000

    Browser Tests

--- tests/Browser/BrowserTests.spec.js
+++ tests/Browser/BrowserTests.spec.js
@@ -13,10 +13,18 @@ test('Launch Browser using Page fixture', async ({page}) => {
     await expect(page).toHaveTitle("AskOmDch – Become a Selenium automation expert!");
 });
 
-test.only('Extracting text from a web element', async ({page})=>{
+test('Extracting text from a web element', async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await page.locator("#username").fill("nikhilrao@test.com");
     await page.locator("#password").fill("password");
     await page.locator("#signInBtn").click();
     console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
+})
+
+test('Extracting text from a link web element', async ({page})=>{
+    await page.goto("https://askomdch.com");
+    await page.locator("li[id='menu-item-1227'] a").click();
+    console.log(await page.locator(".woocommerce-products-header__title.page-title").textContent());
+    await expect(page.locator(".woocommerce-products-header__title.page-title")).toContainText("Store");
 })
