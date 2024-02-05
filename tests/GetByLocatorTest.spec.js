c0ef58129085ca4976aafd24ab20675c93b2b14c
Nikhil <nikhilcogni@gmail.com>
2024-02-05 00:09:11 +0000

    Code for GetByLocators

---
+++ tests/GetByLocatorTest.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+test('Demo on usage of GetBy locators', async ({page}) => {
+    
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Male");
+    await page.locator('form input[name="name"]').fill("Rahul");
+    await page.getByPlaceholder("Password").fill("password");
+    await page.getByRole('button',{name:"Submit"}).click();
+    await page.getByLabel("Success! The Form has been submitted successfully!.").isVisible();
+   
+    await page.getByRole('link',{name:"Shop"}).click();
+    await page.locator("app-card").filter({hasText:"iphone X"}).getByRole('button',{name:"Add"}).click();
+});
