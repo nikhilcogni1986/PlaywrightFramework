19d188ea12c8e96bb0e71bc5194d2636c6ba0f98
Nikhil <nikhilcogni@gmail.com>
2024-02-06 01:16:37 +0000

    Dropdown tests

---
+++ tests/BoostrapDropDownTest.spec.js
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test';
+
+test('Select options from Boostrap Drop Down', async({page})=>{
+
+    await page.goto("https://jquery-az.com/boots/demo.php?ex=63.0_2");
+    await page.locator(".multiselect").click();
+
+    //Assert for the number of options
+    await expect(page.locator("ul li label input")).toHaveCount(11);
+
+    //Select the specific option and print the options
+    const options = await page.$$("ul li label");
+    await expect(options.length).toBe(14);
+
+    //Loop through array and print the options
+    for(let option of options)
+    {
+        const value = await option.textContent();
+        await console.log(value);
+
+        if(value.includes('Angular') || value.includes('Java'))
+        {
+            await option.click();
+        }
+    }
+    await page.waitForTimeout(2000)
+});
---
+++ tests/DropDownTest.spec.js
@@ -0,0 +1,19 @@
+import {test, expect} from '@playwright/test';
+
+test('Select options from Multi Select Drop Down', async({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    await page.locator("#colors").selectOption(['Red','Blue']);
+
+    //Assertion to check number of options
+    const options = await page.locator("#colors option").count();
+    await expect(options).toBe(5);
+
+    //Assertion using Javascript
+    const drpdwnOptions = await page.$$("#colors option");
+    await expect(drpdwnOptions.length).toBe(5);
+
+    //Assertion to check presence of an option in drop down
+    const optionContent = await page.locator("#colors").textContent();
+    await expect(optionContent.includes("Blue")).toBeTruthy();
+});
