d7b45462eddb3c331054b19da0e5db69e519867e
Nikhil <nikhilcogni@gmail.com>
2024-02-10 11:55:13 +0000

    Data Drive Tests

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,10 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  reporter: 'html',
+  reporter:  [
+    ['allure-playwright', {outputFolder: 'my-allure-results'}]
+   ],
   fullyParallel: false,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   projects:
   [
     {
@@ -18,19 +19,7 @@ module.exports = defineConfig({
         video:'off',
         viewport:{height:1080, width:1920}
       }
-    },
-    {
-      name: "Safari",
-      use:
-      {
-        browserName:"webkit",
-        headless:true,
-        screenshot: 'only-on-failure',
-        video: 'off'
-      }
     }
-      
   ]
-  
 });
 
---
+++ tests/DataDrivenTest.spec.js
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test';
+const testData = JSON.parse(JSON.stringify(require('../TestData/loginCredentials.json')))
+
+test.describe('Login using different credentials', ()=>
+{
+    for(const userCreds of testData)
+    {
+        test(`Login into application for a ${userCreds.UserType}`, async({page})=>
+        {
+            await page.goto("https://www.saucedemo.com/");
+            
+            await page.locator("#user-name").fill("");
+            await page.locator("#user-name").fill(userCreds.username);
+            
+            await page.locator("#password").fill("");
+            await page.locator("#password").fill(userCreds.password);
+
+            await page.locator("#login-button").click();
+            await page.waitForTimeout(1000);
+            
+            if(userCreds.UserType === 'standard_user')
+                expect(await page.locator(".title").textContent()).toContain(userCreds.expected);
+            else
+            expect(await page.locator("h3[data-test='error']").textContent()).toContain(userCreds.expected);
+        })
+    }
+}); 
